92d8a4d46260ce567e9764746b861dcc46d7e06b
MD-CODE <82255308+naylachan@users.noreply.github.com>
2023-11-02 18:37:42 +0800

    Add files via upload

--- dist/index.js
+++ dist/index.js
@@ -13,10 +13,13 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-console.log(`[ CONNECT ] MdCode • WEB "https://md-code.xyz"`)
 Object.defineProperty(exports, "__esModule", { value: true });
+
+ 
+
 __exportStar(require("./Socket"), exports);
 __exportStar(require("./Messaging"), exports);
 __exportStar(require("./Utils"), exports);
 __exportStar(require("./Types"), exports);
 __exportStar(require("./Error"), exports);
+__exportStar(require("./Database"), exports);
